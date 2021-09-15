e8c4a664cc93e1f40dc572f1d3b3343bd6e183b4
Nathan <n405m437@ku.edu>
2021-09-15 07:58:10 -0500

    nothing was changd just a test commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
   Executive exec;
   exec.run();
-=======
   int ships = 0;
   std::cout << "How many ships would you like?\n";
   std::cin >> ships;
@@ -17,5 +15,4 @@ int main(int argc, char* argv[])
   //testing
   // new test
   //Yuyang test
->>>>>>> 2f0e65df2fda6f6f83c05b6038bb5370f4f16505
 }
